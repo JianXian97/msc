1d023858da341fa2b0c2949b59cdb3487c61e0cc
Jensen Soo <soojianxian@gmail.com>
2023-07-04 00:33:17 +0100

    cft2, ignore hd warnings

--- UNETR/trainer.py
+++ UNETR/trainer.py
@@ -14,6 +14,7 @@ import shutil
 import time
 import warnings
 warnings.filterwarnings("ignore", message="the ground truth of class ") #HD distance warning ignore
+warnings.filterwarnings("ignore", message="the prediction of class ") #HD distance warning ignore
 
 import numpy as np
 import torch
